53ca6498b64b30a25c9695cedd7d3b4a718510c4
M-Hassan11 <mhassan909234@gmail.com>
2024-08-05 07:42:54 -0700

    Initial commit

---
+++ qrCode_generator.py
@@ -0,0 +1,8 @@
+import qrcode
+
+arabic_verses:str = " اِنَّ رَبِّیْ رَحِیْمٌ وَّدُوْدٌ , فَلَا یَحْزُنْكَ قَوْلُهُمْۘ" 
+urdu_transition:str = "یقین مانو کہ میرا ربّ بڑی مہربانی والا اور بہت محبت کرنے والا ہے"
+combined_text = f"{arabic_verses}\n{urdu_transition}"
+
+qr = qrcode.make(combined_text)
+qr.save("qr.png")
