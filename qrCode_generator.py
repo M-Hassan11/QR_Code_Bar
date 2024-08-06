0e9b1cf5dc30670ba845b792191f978f5ffef4e6
M-Hassan11 <mhassan909234@gmail.com>
2024-08-05 21:41:43 -0700

    changed qr code

--- qrCode_generator.py
+++ qrCode_generator.py
@@ -1,6 +1,6 @@
 import qrcode
 
-arabic_verses:str = " اِنَّ رَبِّیْ رَحِیْمٌ وَّدُوْدٌ , فَلَا یَحْزُنْكَ قَوْلُهُمْۘ" 
+arabic_verses:str = "اِنَّ رَبِّیْ رَحِیْمٌ وَّدُوْدٌ" 
 urdu_transition:str = "یقین مانو کہ میرا ربّ بڑی مہربانی والا اور بہت محبت کرنے والا ہے"
 combined_text = f"{arabic_verses}\n{urdu_transition}"
 
